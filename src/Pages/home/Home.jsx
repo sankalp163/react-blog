f4a863c1ebcbed0adaeea9ac58b80ad35b09e244
Sankalp Saxena <sankalp1632000@gmail.com>
2021-07-08 17:26:02 +0530

    Set up and Navbar files added/modified

---
+++ src/Pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Topbar from '../../Components/topbar/Topbar';
+
+
+function Home() {
+    return (
+        <div>
+            <Topbar />
+        </div>
+    )
+}
+
+export default Home;
